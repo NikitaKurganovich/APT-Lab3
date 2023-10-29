8e8cd66fef26d5da6bf83a257070b1ee0b850aba
Nikita Kurhanovich <babananick@gmail.com>
2023-10-29 15:44:17 +0300

    Added json reader

--- app/src/main/java/com/example/aptlab3/MainActivity.kt
+++ app/src/main/java/com/example/aptlab3/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -27,41 +28,71 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.aptlab3.repository.CountriesQuestionsRepository
 import com.example.aptlab3.ui.theme.APTLab3Theme
+import org.json.JSONObject
+import java.io.InputStream
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val jsonString = """
+{
+  "questions": [
+    {
+      "question": "Which country this flag is?",
+      "question_type": "with_image",
+      "image": "R.drawable.latvia",
+      "correct_answer": "Latvia",
+      "available_answers": [
+        "Latvia",
+        "Germany",
+        "France",
+        "Nigeria"
+      ]
+    }
+  ]
+}
+"""
+            val jsonObject = JSONObject(jsonString)
+
+            val questions = jsonObject.getJSONArray("questions")
+            val questionObject = questions.getJSONObject(0)
             APTLab3Theme {
+
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    val list = remember { mutableStateOf(listOf("A", "B", "C", "D")) }
+                    val variantList = mutableListOf<String>()
+                    for (i in 0..questionObject.getJSONArray("available_answers").length() - 1) {
+                        variantList.add(questionObject.getJSONArray("available_answers")[i].toString())
+                    }
+                    val defList: List<String> = variantList
+                    val list = remember {
+                        mutableStateOf(defList)
+                    }
                     val shape = RoundedCornerShape(10.dp)
                     Column {
+                        Image(
+                            painter = painterResource(id = R.drawable.latvia), contentDescription = "Latvia"
+                        )
                         Button(onClick = {
                             list.value = list.value.shuffled()
                         }) {
                             Text("Shuffle")
                         }
-                        // Display the list
                         LazyVerticalGrid(
                             columns = GridCells.Fixed(2),
-                        ){
-                            items(list.value){
+                        ) {
+                            items(list.value) {
                                 Text(
-                                    it,
-                                    Modifier
-                                        .align(Alignment.CenterHorizontally)
-                                        .border(
-                                            BorderStroke(2.dp, Color.Black),
-                                            shape = shape
-                                        )
-                                        .padding(all = 3.dp),
-                                    style = TextStyle(
+                                    it, Modifier.align(Alignment.CenterHorizontally).border(
+                                            BorderStroke(2.dp, Color.Black), shape = shape
+                                        ).padding(all = 3.dp), style = TextStyle(
                                         fontSize = 30.sp
                                     )
                                 )
@@ -77,8 +108,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = "Hello $name!", modifier = modifier
     )
 }
 
