1fa084a827347de3f892ced7c50c0c84fe620833
Babananick <babananick@gmail.com>
2023-11-19 23:06:58 +0300

    Made buttons don't clickable after first choose

--- app/src/main/java/com/example/aptlab3/model/Question.kt
+++ app/src/main/java/com/example/aptlab3/model/Question.kt
@@ -1,13 +1,13 @@
 package com.example.aptlab3.model
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
@@ -15,6 +15,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.aptlab3.ui.theme.APTLab3Theme
 import org.json.JSONObject
 
 open class Question(private val jsonObject: JSONObject) {
@@ -33,31 +34,38 @@ open class Question(private val jsonObject: JSONObject) {
     }
     @Composable
     fun AnswerVariants() {
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(2)
-        ) {
-            items(answersVariants) {
-                var color by remember(it) { mutableStateOf(Color.Blue) }
-                Button(
-                    onClick = {
-                        color = if (correctAnswer == it){
-                            Color.Green
-                        } else{
-                            Color.Red
-                        }
-                    },
-                    Modifier
-                        .padding(all = 3.dp),
-                    colors = ButtonDefaults.buttonColors(color)
-                ){
-                    Text(
-                        it, style = TextStyle(
-                            fontSize = 25.sp
+        var isClickable by remember(this){ mutableStateOf(true)}
+        APTLab3Theme{
+            val buttonColor = colorScheme.primary
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2)
+            ) {
+                items(answersVariants) {
+                    var color by remember(it) { mutableStateOf(buttonColor) }
+                    Button(
+                        enabled = isClickable,
+                        onClick = {
+                            isClickable = false
+                            color = if (correctAnswer == it){
+                                Color.Green
+                            } else {
+                                Color.Red
+                            }
+                        },
+                        modifier = Modifier
+                            .padding(all = 3.dp),
+                        colors = ButtonDefaults.buttonColors(containerColor = color,disabledContainerColor = color)
+                    ){
+                        Text(
+                            it, style = TextStyle(
+                                fontSize = 25.sp
+                            )
                         )
-                    )
+                    }
                 }
             }
         }
+
     }
     private val answers = List(jsonObject.getJSONArray("available_answers").length()) { i ->
         jsonObject.getJSONArray("available_answers")[i].toString()
