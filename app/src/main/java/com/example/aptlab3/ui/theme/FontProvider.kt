6de8256bf28132b1d982a0f36f9802632596a226
Babananick <babananick@gmail.com>
2023-11-21 20:06:15 +0300

    Added custom font

---
+++ app/src/main/java/com/example/aptlab3/ui/theme/FontProvider.kt
@@ -0,0 +1,17 @@
+package com.example.aptlab3.ui.theme
+
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.googlefonts.Font
+import androidx.compose.ui.text.googlefonts.GoogleFont
+import com.example.aptlab3.R
+
+val provider = GoogleFont.Provider(
+    providerAuthority = "com.google.android.gms.fonts",
+    providerPackage = "com.google.android.gms",
+    certificates = R.array.com_google_android_gms_fonts_certs
+
+)
+
+val montserratFont = FontFamily(
+    Font(googleFont = GoogleFont("Montserrat"), fontProvider = provider)
+)
--- app/src/main/java/com/example/aptlab3/ui/theme/Type.kt
+++ app/src/main/java/com/example/aptlab3/ui/theme/Type.kt
@@ -4,12 +4,14 @@ import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.googlefonts.Font
+import androidx.compose.ui.text.googlefonts.GoogleFont
 import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = montserratFont,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
